53360694cfafb048fe97519e109fb569a3d10ccb
Gilberto <124392451+eumorales@users.noreply.github.com>
2023-08-16 01:42:15 -0300

    v1.1

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const elementoConsumido = document.getElementById('total');
 const botaoTotal = document.getElementById('string-total');
 const botaoBeber = document.getElementById('maisAgua');
 const botaoResetar = document.getElementById('menosAgua');
+const elementoMetaDiaria = document.getElementById('metaDiaria');
 
 let quantidadeConsumida = parseInt(localStorage.getItem('quantidadeConsumida')) || 0;
-let metaDiaria = 2500;
+let metaDiaria = 2500; 
+
 const tamanhoGarrafa = 500;
 
 const urlParams = new URLSearchParams(window.location.search);
@@ -13,6 +15,14 @@ const metaDiariaParam = urlParams.get('metadiaria');
 if (metaDiariaParam) {
     metaDiaria = parseInt(metaDiariaParam);
 }
+
+var parabens = ""; 
+
+var litrosm = metaDiaria / 1000;
+
+elementoConsumido.textContent = litrosm.toLocaleString(undefined, { minimumFractionDigits: 1, maximumFractionDigits: 1 }) + " Litros" + parabens;
+elementoMetaDiaria.textContent = (metaDiaria / 1000).toLocaleString(undefined, { minimumFractionDigits: 1, maximumFractionDigits: 1 }) + " Litros";
+
 
 function updateDiario() {
     const hoje = new Date();
